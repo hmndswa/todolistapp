383507503dbe9843fd824f79de111abaa40864a7
hasith@wireapps <hasithwireapps@Hasiths-MacBook-Pro.local>
2025-09-01 13:59:58 +0100

    Added Abstract User Model Functionality

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()     # creates the user in PostgreSQL (auth_user)
-            login(request, user)   # auto-login after signup
+            user = form.save()
+            login(request, user)
             return redirect('tdlapp:list_todos')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
 
     return render(request, 'accounts/signup.html', {'form': form})
